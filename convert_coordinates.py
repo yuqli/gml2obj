9e9981a0d8679e5be4f0a91f8b6800b4c9f8e448
Tanay Varshney <varshneytanay@gmail.com>
2019-07-01 16:15:27 -0400

    Conversion Fixed

--- convert_coordinates.py
+++ convert_coordinates.py
@@ -6,12 +6,13 @@ class GeoProcessing:
         self.input_dir = path
 
     def convert_to_lat_long(self):
-        from pyproj import Proj
+        from pyproj import Proj, transform
         from itertools import islice 
         import glob
         import csv
 
-        pnyc = Proj(proj='lcc', datum='NAD83', lat_1=40.666667, lat_2=41.033333, lat_0=40.166667, lon_0=-74.0, x_0=984250.0, y_0=0.0)
+        inProj = Proj(init='epsg:2263', preserve_units = True)
+        outProj = Proj(init='epsg:4326')
         
         with open('point_cloud.csv', mode='w') as pcf:
             pcw = csv.writer(pcf, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -21,10 +22,9 @@ class GeoProcessing:
                     line = next(islice(fin, 2, 3))
                     words = line.split(' ')
                     northing_local,easting_local = words[-2],words[-1]
-                    easting,northing = pnyc(northing_local,easting_local,inverse=True)
                     
+                    easting,northing = transform(inProj,outProj,northing_local,easting_local)                    
                     pcw.writerow([file.split("/")[-1], northing, easting])
-                    #pcw.writerow([file.split("/")[-1].split('.')[0]+".npy", northing, easting])
 
     def fetch_roi_files(self):
         pass
